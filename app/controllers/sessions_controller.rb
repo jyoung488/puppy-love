bccbbae58d339f91660a7c757b1a8bbcfc899496
jyoung488 <jyoung488@gmail.com>
2016-12-23 00:21:50 -0800

    Edit login redirect page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ post '/login' do
     if @user && @user.authenticate(params[:password])
       login(@user)
 
-      erb :index, layout: false
+      redirect '/'
     else
       @error = "Email and/or password are invalid!"
       erb :'/login', layout: false
