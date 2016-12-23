8b23c93e8dae00080ec7f53891b891448777e423
jyoung488 <jyoung488@gmail.com>
2016-12-23 00:33:50 -0800

    Fix user login and register routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ post '/login' do
     if @user && @user.authenticate(params[:password])
       login(@user)
 
-      redirect "/users/#{@user.id}"
+      # redirect "/users/#{@user.id}"
     else
       @error = "Email and/or password are invalid!"
       erb :'/login', layout: false
